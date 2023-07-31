5ecb5e0c7948332c9873653e6954e7f4ceccd45a
Ares9505 <arielic1995@gmail.com>
2023-07-31 12:47:44 -0700

    Actualice .gitignore to exclude config, credentials and .exe and initializing terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,25 @@
+variable current_path{
+  type = string
+  default = "%CD%" /*Linux: "${path,module}"  */
+}
+variable profile{
+  type = string
+  default = "devares" /*your profile name*/
+}
+
 terraform {
   required_providers {
-    aws = "hashicorp/aws"
+    aws = {
+    source = "hashicorp/aws"
     version = "~> 4.16"
+    }
   }
 
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  shared_config_files      = ["/Users/ariel/.aws/conf"]
-  shared_credentials_files = ["/Users/ariel/.aws/creds"]
-  profile                  = "customprofile"
+  shared_config_files      = ["{var.current_path}/../config"]
+  shared_credentials_files = ["{var.current_path}/../credentials"]
+  profile                  = "devares"
 }
